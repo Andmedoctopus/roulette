4ac6eaea7d27d14e010cf17e4dd87100617164df
Andmedoctopus <43909845+Andmedoctopus@users.noreply.github.com>
2019-02-13 12:52:17 +0300

    Update roulet.py

--- roulet.py
+++ roulet.py
@@ -7,20 +7,20 @@ import random
 '''
 from tabulate import tabulate
 
-default_bank = 100
+default_bank = 100   #initial bank that we have
 bank = default_bank
 red = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27 ,30 ,32 ,34 ,36]
-black = [elem for elem in range (1,36) if elem not in red ]
+black = [elem for elem in range (1,36) if elem not in red ] #chose the numbers that not in red
 trylist = [['Red','Black','Zero','Bank', 'Max bet','Default_bank']]
 
 while (bank < 1000000):
     for coun_try in range(0,9):
-        t_red = 0
-        t_black = 0
-        t_zero = 0
-        bank = default_bank
-        bet = 1
-        default_bet = 1
+        t_red = 0 #total count of red
+        t_black = 0 #total count of black
+        t_zero = 0 #total count of zero
+        bank = default_bank 
+        bet = 1 
+        default_bet = 1 #initial bet (not changeble)
         bet_color = 1 #-1 - red, 1 - black
         maxbet = 1
     
@@ -28,7 +28,7 @@ while (bank < 1000000):
             curr_cell = random.randint(0,36)
             if bank <= 0:
                 break    
-            bank -= bet
+            bank -= bet #bet acceptance 
         
             if curr_cell in red:
                 t_red += 1
@@ -58,4 +58,4 @@ while (bank < 1000000):
 for x in range(0,9):
     print(trylist[x])
 
-print(tabulate(trylist))
+print(tabulate(trylist)) #print more readable table 
